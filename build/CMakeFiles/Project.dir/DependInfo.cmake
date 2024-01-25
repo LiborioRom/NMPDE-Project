b2cdaffec2112adf8a6407ceccfe4b506d2ffc34
KamilHanna <131144516+KamilHanna@users.noreply.github.com>
2024-01-25 13:53:01 +0000

    Edited code with condition number printed in csv

---
+++ build/CMakeFiles/Project.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/Desktop/KamilREPO/NMPDE-Project/src/Poisson3D.cpp" "CMakeFiles/Project.dir/src/Poisson3D.cpp.o" "gcc" "CMakeFiles/Project.dir/src/Poisson3D.cpp.o.d"
+  "/home/ubuntu/Desktop/KamilREPO/NMPDE-Project/src/Project.cpp" "CMakeFiles/Project.dir/src/Project.cpp.o" "gcc" "CMakeFiles/Project.dir/src/Project.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Project_parallel.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_parallel.dir/src/Poisson3D_Parallel.cpp.o"
+  "CMakeFiles/Project_parallel.dir/src/Poisson3D_Parallel.cpp.o.d"
+  "CMakeFiles/Project_parallel.dir/src/Project_Parallel.cpp.o"
+  "CMakeFiles/Project_parallel.dir/src/Project_Parallel.cpp.o.d"
+  "Project_parallel"
+  "Project_parallel.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_parallel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
